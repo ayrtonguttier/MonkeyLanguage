4fde4ef2c8c7e93b11f645864ea71caa781b233b
Ayrton Guttier <ayrtonsg@hotmail.com>
2024-03-28 21:52:55 -0300

    added keywords and identifiers

--- src/lexer/lexer_test.go
+++ src/lexer/lexer_test.go
@@ -5,23 +5,76 @@ import (
 	"testing"
 )
 
+type tokenValues struct {
+	expectedType    token.TokenType
+	expectedLiteral string
+}
+
 func TestNextToken(t *testing.T) {
-	input := "=+(){},;"
+	input := `
 
-	tests := []struct {
-		expectedType token.TokenType
+    let five = 5;
 
-		expectedLiteral string
-	}{
+    let ten = 10;
+
+    let add = fn(x, y){
+        x + y;
+    };
+
+    let result = add(five, ten);
+
+    `
+
+	tests := []tokenValues{
+		{token.LET, "let"},
+		{token.IDENT, "five"},
 		{token.ASSIGN, "="},
-		{token.PLUS, "+"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
 		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
 		{token.RPAREN, ")"},
 		{token.LBRACE, "{"},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.SEMICOLON, ";"},
 		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+
+		{token.LET, "let"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "add"},
+		{token.LPAREN, "("},
+		{token.IDENT, "five"},
 		{token.COMMA, ","},
+		{token.IDENT, "ten"},
+		{token.RPAREN, ")"},
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
+
+		//	{token.PLUS, "+"},
+		//	{token.LPAREN, "("},
+		//	{token.RPAREN, ")"},
+		//	{token.LBRACE, "{"},
+		//	{token.RBRACE, "}"},
+		//	{token.COMMA, ","},
+		//	{token.SEMICOLON, ";"},
+		//	{token.EOF, ""},
 	}
 
 	l := New(input)
@@ -34,7 +87,7 @@ func TestNextToken(t *testing.T) {
 		}
 
 		if tok.Literal != tt.expectedLiteral {
-            t.Fatalf("test[%d] wrong literal. expected: %q, got: %q", i, tt.expectedLiteral, tok.Literal)
+			t.Fatalf("test[%d] wrong literal. expected: %q, got: %q", i, tt.expectedLiteral, tok.Literal)
 		}
 	}
 
