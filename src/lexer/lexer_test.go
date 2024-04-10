06edf92a4573cfc1ce140a481018c97dd38c5d4b
= <ayrtonsg@hotmail.com>
2024-04-10 16:14:12 -0300

    added some tokens and keywords

--- src/lexer/lexer_test.go
+++ src/lexer/lexer_test.go
@@ -12,18 +12,25 @@ type tokenValues struct {
 
 func TestNextToken(t *testing.T) {
 	input := `
+            let five = 5;
+            let ten = 10;
 
-    let five = 5;
+            let add = fn(x, y) {
+              x + y;
+            };
 
-    let ten = 10;
+            let result = add(five, ten);
+            !-/*5;
+            5 < 10 > 5;
 
-    let add = fn(x, y){
-        x + y;
-    };
+            if (5 < 10) {
+                return true;
+            } else {
+                return false;
+            }
 
-    let result = add(five, ten);
-
-    `
+            10 == 10;
+            10 != 9;`
 
 	tests := []tokenValues{
 		{token.LET, "let"},
@@ -31,13 +38,11 @@ func TestNextToken(t *testing.T) {
 		{token.ASSIGN, "="},
 		{token.INT, "5"},
 		{token.SEMICOLON, ";"},
-
 		{token.LET, "let"},
 		{token.IDENT, "ten"},
 		{token.ASSIGN, "="},
 		{token.INT, "10"},
 		{token.SEMICOLON, ";"},
-
 		{token.LET, "let"},
 		{token.IDENT, "add"},
 		{token.ASSIGN, "="},
@@ -54,7 +59,6 @@ func TestNextToken(t *testing.T) {
 		{token.SEMICOLON, ";"},
 		{token.RBRACE, "}"},
 		{token.SEMICOLON, ";"},
-
 		{token.LET, "let"},
 		{token.IDENT, "result"},
 		{token.ASSIGN, "="},
@@ -65,16 +69,44 @@ func TestNextToken(t *testing.T) {
 		{token.IDENT, "ten"},
 		{token.RPAREN, ")"},
 		{token.SEMICOLON, ";"},
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.IF, "if"},
+		{token.LPAREN, "("},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.TRUE, "true"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.ELSE, "else"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.FALSE, "false"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.INT, "10"},
+		{token.EQ, "=="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "10"},
+		{token.NOT_EQ, "!="},
+		{token.INT, "9"},
+		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
-
-		//	{token.PLUS, "+"},
-		//	{token.LPAREN, "("},
-		//	{token.RPAREN, ")"},
-		//	{token.LBRACE, "{"},
-		//	{token.RBRACE, "}"},
-		//	{token.COMMA, ","},
-		//	{token.SEMICOLON, ";"},
-		//	{token.EOF, ""},
 	}
 
 	l := New(input)
--- src/token/token.go
+++ src/token/token.go
@@ -20,6 +20,14 @@ const (
 
 	ASSIGN    = "="
 	PLUS      = "+"
+
+    MINUS = "-"
+    BANG = "!"
+    ASTERISK ="*"
+    SLASH = "/"
+    LT = "<"
+    GT = ">"
+
 	COMMA     = ","
 	SEMICOLON = ";"
 	LPAREN    = "("
@@ -27,15 +35,28 @@ const (
 	LBRACE    = "{"
 	RBRACE    = "}"
 
+    EQ = "=="
+    NOT_EQ = "!="
+
 	//keywords
 
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
 
 var keywords = map[string]Type{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
 }
 
 func LookupIdent(ident string) Type {
